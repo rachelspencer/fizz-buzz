09a4bd06502c1674cdc3fce5f4fe66244ae4d0e5
Rachel Spencer <rachelspencer688@gmail.com>
2022-08-25 21:05:51 -0400

    Final

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,19 @@
 // Write your solution below this line:
+// Loop from 1 to 50 (inclusive). Each time through, if the number is evenly divisible by 3, say ‘fizz’. 
+// If the number is evenly divisible by 5, say ‘buzz’. 
+// If the number is evenly divisible by both 3 and 5, say ‘fizzbuzz’. 
+// Otherwise, say the number.
+
+
+for (let num = 1; num < 51; num++){
+
+    if (num % 3 === 0 && num % 5 === 0){
+        console.log("fizzbuzz");
+    } else if (num % 3 === 0){
+        console.log("fizz");
+    } else if ( num % 5 === 0){
+        console.log("buzz");
+    } else {
+        console.log(num);
+    }
+}
